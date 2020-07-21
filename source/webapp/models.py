03b2b3f231e5830b72d1ff02d8af137948b6b477
talgar <otalgarbek@gmail.com>
2020-07-21 21:58:24 +0600

    добавлена миграция, статик файл

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
+
+class Article(models.Model):
+    description = models.CharField(max_length=3000, null=False, blank=False, verbose_name='Описание')
+    status = models.CharField(max_length=3000, null=False, blank=False, verbose_name='Статус', default='new' ,choices=status_choices)
+    date_end = models.DateField(verbose_name='Дата выполнения')
+
+
+    def __str__(self):
+        return "{}. {}".format(self.pk, self.description)
