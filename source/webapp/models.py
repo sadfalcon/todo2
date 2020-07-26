717a732c643a8569bfb9272983396a7870c9e265
talgar <otalgarbek@gmail.com>
2020-07-26 17:10:01 +0600

    в табл добавлено новое текстовое поле. применена миграция

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -5,6 +5,7 @@ status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),
 
 class Article(models.Model):
     description = models.CharField(max_length=3000, null=False, blank=False, verbose_name='Описание')
+    full_description = models.TextField(max_length=3000, null=True, blank=True, default = None,verbose_name='Полное описание')
     status = models.CharField(max_length=3000, null=False, blank=False, verbose_name='Статус', default='new' ,choices=status_choices)
     date_end = models.DateField(null=True, blank=True, default = None, verbose_name='Дата выполнения')
 
