b02c747697ffcdde85559819b785a0f4fb36d680
talgar <otalgarbek@gmail.com>
2020-07-29 21:05:29 +0600

    формат даты прикручен

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -7,5 +7,5 @@ default_status = status_choices[0][0]
 class ArticleForm(forms.Form):
     description = forms.CharField(max_length=200, required=True, label='Описание')
     full_description = forms.CharField(max_length=3000, label='Полное описание', widget=forms.Textarea)
-    date_end = forms.DateField(label='Дата выполнения', required=False, widget=forms.DateTimeInput(attrs={'type': 'datetime'}))
+    date_end = forms.DateField(label='Дата выполнения', required=False, widget=forms.DateTimeInput(attrs={'type':'date'}))
     status = forms.ChoiceField(choices=status_choices, initial=default_status, label='статус')
