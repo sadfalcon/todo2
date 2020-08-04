4da89884af29499194b753191359cf803009142f
talgar <otalgarbek@gmail.com>
2020-08-05 01:06:42 +0600

    созданы модели задачи, статуса и типа

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -2,14 +2,49 @@ from django.db import models
 from datetime import datetime, date
 
 # Create your models here.
-status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
+STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Выполнено')]
+TYPE_CHOICES = [('task', 'Задача'), ('bug', 'Ошибка'),  ('enhancement', 'Улучшение')]
 
-class Article(models.Model):
-    description = models.CharField(max_length=3000, null=False, blank=False, verbose_name='Описание')
-    full_description = models.TextField(max_length=3000, null=True, blank=True, default = None,verbose_name='Полное описание')
-    status = models.CharField(max_length=3000, null=False, blank=False, verbose_name='Статус', default='new' ,choices=status_choices)
-    date_end = models.DateField(null=True, blank=True, default = None, verbose_name='Дата выполнения')
+
+class Task(models.Model):
+    summary = models.CharField(max_length=3000, null=False, blank=False, verbose_name='Описание')
+    description = models.TextField(max_length=3000, null=True, blank=True, default=None, verbose_name='Полное описание')
+    status = models.ForeignKey('webapp.Status', related_name='tasks',
+                                on_delete=models.CASCADE, verbose_name='Статус')
+    types = models.ForeignKey('webapp.Types', related_name='tasks',
+                               on_delete=models.CASCADE, verbose_name='Тип')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
 
 
     def __str__(self):
-        return "{}. {}".format(self.pk, self.description)
+        return "{}. {}".format(self.pk, self.summary)
+
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+class Status(models.Model):
+    name = models.CharField(max_length=15, choices=STATUS_CHOICES, default='new', verbose_name='Модерация')
+
+
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+
+class Types(models.Model):
+    name = models.CharField(max_length=15, choices=TYPE_CHOICES, default='new', verbose_name='Модерация')
+
+    class Meta:
+        verbose_name = 'Тип'
+        verbose_name_plural = 'Типы'
+
+
+
+
+
+
+
+
+
