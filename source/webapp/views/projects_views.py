c617b390bc4b988b3cb95a86e4d27d05239ac585
talgar <otalgarbek@gmail.com>
2020-08-22 15:37:26 +0600

    добавлена пагинация задач

--- source/webapp/views/projects_views.py
+++ source/webapp/views/projects_views.py
@@ -4,7 +4,7 @@ from webapp.models import Task, Status, Types, TYPE_CHOICES, STATUS_CHOICES, Pro
 from webapp.forms import TaskForm, ProjectForm
 from django.http import HttpResponseNotAllowed
 from django.views.generic import CreateView, ListView, DetailView
-
+from django.core.paginator import Paginator
 from django.db.models import Q
 
 
@@ -20,13 +20,29 @@ class ProjectsIndex(ListView):
 class ProjectView(DetailView):
     template_name = 'project/project_view.html'
     model = Projects
+    paginate_tasks_by = 2
+    paginate_tasks_orphans = 0
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        project = self.object
-        tasks = project.tasks.all()
+        tasks, page, is_paginated = self.paginate_tasks(self.object)
         context['tasks'] = tasks
+        context['page_obj'] = page
+        context['is_paginated'] = is_paginated
         return context
+
+    def paginate_tasks(self, project):
+        tasks = project.tasks.all().order_by('-created_at')
+        if tasks.count() > 0:
+            paginator = Paginator(tasks, self.paginate_tasks_by, orphans=self.paginate_tasks_orphans)
+            page_number = self.request.GET.get('page', 1)
+            page = paginator.get_page(page_number)
+            is_paginated = paginator.num_pages > 1  # page.has_other_pages()
+            return page.object_list, page, is_paginated
+        else:
+            return tasks, None, False
+
+
 
 class ProjectCreateView(CreateView):
     template_name = 'project/project_create.html'
